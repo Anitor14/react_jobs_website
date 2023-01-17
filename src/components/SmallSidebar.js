36c9921593ba1d0dc7328e804051694a022b96e8
Anitor <anitorabraham@gmail.com>
2023-01-17 22:52:43 +0100

    implemented small sidebar toggle functionality

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,10 +1,31 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import links from "../utils/link";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
 
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>Small Sidebar</h4>
+      <div
+        //   conditionally rendering based on the showSidebar.
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">nav links</div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
