710a27eb3f196c043943dcb22d3a5e93940d21fb
Anitor <anitorabraham@gmail.com>
2023-02-20 16:51:04 +0100

    worked on the area and bar chart

--- src/components/StatsItem.js
+++ src/components/StatsItem.js
@@ -1,5 +1,15 @@
-const StatsItem = () => {
-  return <div>StatsItem</div>;
+import Wrapper from "../assets/wrappers/StatItem";
+
+const StatsItem = ({ count, title, icon, color, bcg }) => {
+  return (
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className="count">{count}</span>
+        <div className="icon">{icon}</div>
+      </header>
+      <h5 className="title">{title}</h5>  
+    </Wrapper>
+  );
 };
 
 export default StatsItem;
